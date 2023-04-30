8811ef1dac0503bb66eda4b34b6f4816cb699a9c
Adam-Riojas <adam_riojas@hotmail.com>
2023-04-30 16:14:43 -0500

    employee data and department

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,21 +1,20 @@
 <script setup>
-    import { onMounted } from 'vue' 
-    import Mainsearch from '@/components/Mainsearch.vue'
-    import MainCards from '@/components/MainCards.vue'
-    import Mainpagination from '@/components/MainPagination.vue'
+import { onMounted } from 'vue'
+import Mainsearch from '@/components/Mainsearch.vue'
+import MainCards from '@/components/MainCards.vue'
+import Mainpagination from '@/components/MainPagination.vue'
 
-    import useAPI from '@/composables/useAPI'
-    
-    const { getEmployees, loading } = useAPI() 
+import useAPI from '@/composables/useAPI'
 
+const { getEmployees, loading } = useAPI()
 
-    onMounted(async () => {
-          await getEmployees()
-        })
+onMounted(async () => {
+  await getEmployees()
+})
 </script>
 
 <template>
-  <p v-if="loading">Loading... </p>
+  <p v-if="loading">Loading...</p>
   <div v-else class="wrapper">
     <Mainsearch />
     <MainCards />
@@ -24,7 +23,7 @@
 </template>
 
 <style scoped lang="postcss">
-  .wrapper {
-    @apply container mx-auto flex flex-col gap-4 py-4;
-  }
+.wrapper {
+  @apply container mx-auto flex flex-col gap-4 py-4;
+}
 </style>
