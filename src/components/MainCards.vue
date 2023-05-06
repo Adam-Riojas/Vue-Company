c38413b9feb85e5d1e99aedba1f0cd1dcfa61da4
Adam-Riojas <adam_riojas@hotmail.com>
2023-05-05 21:39:35 -0500

    updating main cards

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,15 +1,17 @@
 <script setup>
 import useAPI from '@/composables/useAPI'
 import MainCardsSingle from '@/components/MainCardsSingle.vue'
-
-
 const { employees } = useAPI()
 </script>
 
 <template>
   <div class="sub-wrapper">
     <Suspense>
-      <MainCardsSingle v-for="employee in employees" :key="employee.employeeId" :employee="employee" />
+      <MainCardsSingle
+        v-for="employee in employees"
+        :key="employee.employeeId"
+        :employee="employee"
+      />
       <template #fallback>
         <div>Loading...</div>
       </template>
@@ -18,7 +20,7 @@ const { employees } = useAPI()
 </template>
 
 <style scoped lang="postcss">
-  .sub-wrapper {
-    @apply grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4;
-  }
+.sub-wrapper {
+  @apply grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4;
+}
 </style>
